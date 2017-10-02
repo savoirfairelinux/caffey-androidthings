dfd5d652a18de3b734ea8e6bfad661822744e59e
Hadrien De Sousa <hadrien.desousa@savoirfairelinux.com>
2017-10-02 13:25:19 -0400

    Add software to hardware logic

--- app/src/main/java/com/savoirfairelinux/caffey/MainActivity.kt
+++ app/src/main/java/com/savoirfairelinux/caffey/MainActivity.kt
@@ -1,32 +1,118 @@
 package com.savoirfairelinux.caffey
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.widget.SeekBar
+import com.google.android.things.contrib.driver.apa102.Apa102
+import com.google.android.things.contrib.driver.button.Button
+import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.util.*
 
-/**
- * Skeleton of an Android Things activity.
- *
- * Android Things peripheral APIs are accessible through the class
- * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
- * set it to HIGH:
- *
- * <pre>{@code
- * val service = PeripheralManagerService()
- * val mLedGpio = service.openGpio("BCM6")
- * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
- * mLedGpio.value = true
- * }</pre>
- * <p>
- * For more complex peripherals, look for an existing user-space driver, or implement one if none
- * is available.
- *
- * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
- *
- */
-class MainActivity : Activity() {
+class MainActivity : Activity(), SeekBar.OnSeekBarChangeListener {
+
+    private val TAG = MainActivity::class.java.simpleName
+
+    // Default LED brightness
+    private val LEDSTRIP_BRIGHTNESS = 1
+
+    private lateinit var mDisplay: AlphanumericDisplay
+    private lateinit var mLedstrip: Apa102
+
+    private lateinit var buttonA: Button
+    private lateinit var buttonB: Button
+    private lateinit var buttonC: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        seekBarSugar.max = MAX_SUGAR
+        seekBarSugar.setOnSeekBarChangeListener(this)
+
+        // Initialize 7-segment display
+        try {
+            mDisplay = RainbowHat.openDisplay()
+            mDisplay.setEnabled(true)
+            mDisplay.display("1234")
+            Log.d(TAG, "Initialized I2C Display")
+        } catch (e: IOException) {
+            throw RuntimeException("Error initializing display", e)
+        }
+
+        // Initialize LED strip
+        try {
+            mLedstrip = RainbowHat.openLedStrip()
+            mLedstrip.brightness = LEDSTRIP_BRIGHTNESS
+            val colors = IntArray(7)
+            Arrays.fill(colors, Color.RED)
+            mLedstrip.write(colors)
+            // Because of a known APA102 issue, write the initial value twice.
+            mLedstrip.write(colors)
+
+            Log.d(TAG, "Initialized SPI LED strip")
+        } catch (e: IOException) {
+            throw RuntimeException("Error initializing LED strip", e)
+        }
+
+        // Initialize buttons
+        try {
+            buttonA = RainbowHat.openButtonA()
+            buttonB = RainbowHat.openButtonB()
+            buttonC = RainbowHat.openButtonC()
+            buttonA.setOnButtonEventListener { _, _ ->
+                coffeeSize.text = "Petit"
+            }
+            buttonB.setOnButtonEventListener { _, _ ->
+                coffeeSize.text = "Moyen"
+            }
+            buttonC.setOnButtonEventListener { _, _ ->
+                coffeeSize.text = "Grand"
+            }
+        } catch (e: IOException) {
+            throw RuntimeException("Error initializing LED strip", e)
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        try {
+            mDisplay.clear()
+            mDisplay.setEnabled(false)
+            mDisplay.close()
+        } catch (e: IOException) {
+            Log.e(TAG, "Error closing display", e)
+        }
+
+        try {
+            mLedstrip.brightness = 0
+            mLedstrip.write(IntArray(7))
+            mLedstrip.close()
+        } catch (e: IOException) {
+            Log.e(TAG, "Error closing LED strip", e)
+        }
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+        try {
+            mDisplay.display(progress.toDouble())
+            val colors = RainbowUtil.getWeatherStripColors(progress)
+            mLedstrip.write(colors)
+        } catch (e: IOException) {
+            Log.e(TAG, "Error updating display", e)
+        }
     }
 }
---
+++ app/src/main/java/com/savoirfairelinux/caffey/RainbowUtil.kt
@@ -0,0 +1,49 @@
+package com.savoirfairelinux.caffey
+
+import android.graphics.Color
+
+/**
+ * Created by hdesousa on 02/10/17.
+ */
+/**
+ * Helper methods for computing outputs on the Rainbow HAT
+ */
+
+const val MAX_SUGAR = 100
+
+object RainbowUtil {
+    /* LED Strip Color Constants*/
+    private var sRainbowColors: IntArray = IntArray(7)
+
+    /* Sugar Range */
+    private var sugarRanges: IntArray = IntArray(7)
+
+    init {
+        for (i in sRainbowColors.indices) {
+            val hsv = floatArrayOf(i * 240f / sRainbowColors.size, 1.0f, 1.0f)
+            sRainbowColors[i] = Color.HSVToColor(255, hsv)
+        }
+
+        var sugarRange = MAX_SUGAR / 7
+
+        for (i in sugarRanges.indices) {
+            sugarRanges[i] = sugarRange
+            sugarRange += MAX_SUGAR / 7
+        }
+    }
+
+    /**
+     * Return an array of colors for the LED strip based on the given pressure.
+     * @param pressure Pressure reading to compare.
+     * @return Array of colors to set on the LED strip.
+     */
+    fun getWeatherStripColors(sugarLevel: Int): IntArray {
+        val colors = IntArray(sRainbowColors.size)
+
+        sugarRanges.indices
+                .filter { sugarLevel >= sugarRanges[it] }
+                .forEach { colors[it] = sRainbowColors.reversedArray()[it] }
+
+        return colors.reversedArray()
+    }
+}
