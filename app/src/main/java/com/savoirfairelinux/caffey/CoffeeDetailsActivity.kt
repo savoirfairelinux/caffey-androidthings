950158506197249beef627238d33a901732053f8
Loïc Siret <loic.siret@savoirfairelinux.com>
2017-10-02 12:03:55 -0400

    Add display of the coffee price on seg
    Use the segment display

--- app/src/main/java/com/savoirfairelinux/caffey/CoffeeDetailsActivity.kt
+++ app/src/main/java/com/savoirfairelinux/caffey/CoffeeDetailsActivity.kt
@@ -5,6 +5,11 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import com.savoirfairelinux.caffey.model.Coffee
+import com.google.android.things.contrib.driver.ht16k33.Ht16k33
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat
+import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay
+
+
 
 /**
  * Created by lsiret on 17-10-02.
@@ -12,15 +17,29 @@ import com.savoirfairelinux.caffey.model.Coffee
 fun Context.UserDetailIntent(coffee: Coffee): Intent {
     return Intent(this, CoffeeDetailsActivity::class.java).apply {
         putExtra(INTENT_PRICE, coffee.price)
+        putExtra(INTENT_NAME, coffee.name)
     }
 }
 private const val INTENT_PRICE = "PRICE"
+private const val INTENT_NAME = "NAME"
 
 class CoffeeDetailsActivity : Activity() {
+
+    lateinit var coffee: Coffee;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
-        val userId = intent.getStringExtra(INTENT_PRICE)
-        requireNotNull(userId) { "no price provided in Intent extras" }
+        val price = intent.getIntExtra(INTENT_PRICE,0)
+        val name = intent.getStringExtra(INTENT_NAME)
+        requireNotNull(price) { "no price provided in Intent extras" }
+        requireNotNull(name) { "no name provided in Intent extras" }
+        coffee = Coffee(name, price)
+        val segment = RainbowHat.openDisplay()
+        segment.setBrightness(Ht16k33.HT16K33_BRIGHTNESS_MAX)
+        segment.display(coffee.price)
+        segment.setEnabled(true)
+        // Close the device when done.
+        segment.close()
     }
 }
